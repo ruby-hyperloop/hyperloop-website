c0d753722184ed9c5ab4ad4cd12f9e3dbe2ea20d
Barrie Hadfield <barrie@workshare.com>
2017-08-23 10:46:42 +0100

    Page component

--- app/components/page.rb
+++ app/components/page.rb
@@ -1,13 +1,20 @@
 class Page < Hyperloop::Component
 
-  before_mount do
+  after_mount do
     puts "about to get"
     HTTP.get('https://raw.githubusercontent.com/ruby-hyperloop/hyper-react/master/DOCS.md') do |response|
-      mutate.html response.body
+      @raw = response.body
+      mutate.html `Markdown.toHTML("hello")`
     end
   end
 
   render(DIV) do
-    SPAN { state.html }
+    BUTTON { "do it" }.on(:click) do
+      HTTP.get('https://raw.githubusercontent.com/ruby-hyperloop/hyper-react/master/DOCS.md') do |response|
+        @raw = response.body
+        mutate.html `Markdown.toHTML(#{@raw})`
+      end
+    end
+    DIV(dangerously_set_inner_HTML: { __html: state.html})
   end
 end
