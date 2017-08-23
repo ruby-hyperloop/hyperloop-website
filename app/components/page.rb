461444c8094e5f18ac1aa5043f8ae954b3244809
Barrie Hadfield <barrie@workshare.com>
2017-08-23 16:05:00 +0100

    markdown highlighting  working

--- app/components/page.rb
+++ app/components/page.rb
@@ -1,25 +1,19 @@
+class HighlightX < Hyperloop::Component
+  imports 'Highlight'
+end
+
 class Page < Hyperloop::Component
 
   before_mount do
-    puts "about to get yy"
     HTTP.get('https://raw.githubusercontent.com/ruby-hyperloop/hyper-react/master/DOCS.md') do |response|
       @raw = response.body
       mutate.html `Marked(#{@raw})`
     end
   end
 
-  # state html: ""
-
   render(DIV) do
-    # BUTTON { "do it" }.on(:click) do
-    #   HTTP.get('https://raw.githubusercontent.com/ruby-hyperloop/hyper-react/master/DOCS.md') do |response|
-    #     @raw = response.body
-    #     mutate.html `Marked(#{@raw})`
-    #     mutate.got_it true
-    #   end
-    # end
-    # DIV(dangerously_set_inner_HTML: { __html: state.html })
-    # puts state.html.to_s
-    Highlight(innerHTML: true) { state.html }
+    HighlightX(innerHTML: true) {
+      state.html
+    }
   end
 end
