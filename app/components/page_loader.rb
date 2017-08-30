7e6dffec3b8f482ddc46b91ba7d8d86a542f4bb1
Barrie Hadfield <barrie@workshare.com>
2017-08-30 07:08:27 +0100

    with a store

--- app/components/page_loader.rb
+++ app/components/page_loader.rb
@@ -1,36 +1,5 @@
 class PageLoader < Hyperloop::Component
 
-  after_mount do
-    bfm
-  end
-
-  def bfm
-    @pages = [
-      { repo: 'hyper-store', file: 'DOCS.md', allow_edit: true },
-      { repo: 'hyper-mesh', file: 'DOCS.md', allow_edit: true },
-      { repo: 'hyper-react', file: 'DOCS.md', allow_edit: true }
-    ]
-    load_and_convert_pages
-  end
-
-  def load_and_convert_pages
-    @pages.each do |page|
-      HTTP.get( raw_url(page) ) do |response|
-        puts "response"
-        page[:md_converter] = MdConverter.new(response.body)
-        page[:edit_url] = edit_url page
-      end
-    end
-  end
-
-  def raw_url page
-    "https://raw.githubusercontent.com/ruby-hyperloop/#{page[:repo]}/master/#{page[:file]}"
-  end
-
-  def edit_url page
-    "https://github.com/ruby-hyperloop/#{page[:repo]}/edit/master/#{page[:file]}"
-  end
-
   render(DIV) do
     BUTTON { "Before Mount" }.on(:click) { bfm }
     BUTTON { "Force Update" }.on(:click) { force_update! }
@@ -51,19 +20,19 @@ class PageLoader < Hyperloop::Component
   def side_nav
     # Sem.Container(style: { marginTop: '2em', paddingLeft: '28px' }) {
       Sticky {
-        @pages.each do |page|
+        PageStore.pages.each do |page|
           PageToc(page: page)
-        end if @pages
+        end
       }
     # }
   end
 
   def body
     Sem.Container(style: { marginTop: '2em', paddingLeft: '28px' }) {
-      @pages.each do |page|
+      PageStore.pages.each do |page|
         PageBody(page: page) if page[:md_converter]
         Sem.Divider()
-      end if @pages
+      end
     }
   end
 
---
+++ app/stores/page_store.rb
@@ -0,0 +1,43 @@
+class PageStore < Hyperloop::Store
+
+  class << self
+
+    state pages: nil, scope: :class
+
+    def pages
+      init unless state.pages
+      state.pages
+    end
+
+    def init
+      mutate.pages [
+        { repo: 'hyper-react',     file: 'DOCS.md',  allow_edit: true },
+        { repo: 'hyper-store',     file: 'DOCS.md',  allow_edit: true },
+        { repo: 'hyper-router',    file: 'DOCS.md',  allow_edit: true },
+        { repo: 'hyper-mesh',      file: 'DOCS.md',  allow_edit: true },
+        { repo: 'hyper-operation', file: 'DOCS.md',  allow_edit: true },
+        { repo: 'hyper-operation', file: 'DOCS-POLICIES.md',  allow_edit: true }
+      ]
+      load_and_convert_pages
+    end
+
+    def load_and_convert_pages
+      state.pages.each do |page|
+        page[:md_converter] = nil
+        HTTP.get( raw_url(page) ) do |response|
+          puts "response"
+          page[:md_converter] = MdConverter.new(response.body)
+          page[:edit_url] = edit_url page
+        end
+      end
+    end
+
+    def raw_url page
+      "https://raw.githubusercontent.com/ruby-hyperloop/#{page[:repo]}/master/#{page[:file]}"
+    end
+
+    def edit_url page
+      "https://github.com/ruby-hyperloop/#{page[:repo]}/edit/master/#{page[:file]}"
+    end
+  end
+end
