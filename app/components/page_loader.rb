b1ac1c60cfdc3ef50d24e62cf99ca4e3ae9475cf
fzingg <fredericzingg@protonmail.com>
2017-09-01 16:00:34 +0100

    CSS working

--- app/components/page_loader.rb
+++ app/components/page_loader.rb
@@ -5,16 +5,16 @@ class PageLoader < Hyperloop::Component
 
   render(DIV) do
     if PageStore.loaded?
-      Sem.Grid do
-        Sem.GridRow(columns: 2) do
-          Sem.GridColumn(width: 4) do
+      # Sem.Grid do
+      #   Sem.GridRow(columns: 2) do
+      #     Sem.GridColumn(width: 4) do
             side_nav
-          end
-          Sem.GridColumn(width: 12) do
+          # end
+          # Sem.GridColumn(width: 12) do
             body
-          end
-        end
-      end
+          # end
+        # end
+      # end
     else
       Sem.Dimmer(active:true, inverted:true) {
         Sem.Loader(inverted:true, size: :large, content: 'Loading pages')
@@ -25,21 +25,25 @@ class PageLoader < Hyperloop::Component
 
   def side_nav
     # Sem.Container(style: { marginTop: '2em', paddingLeft: '28px' }) {
-      Sticky {
+      # Sticky {
         # PageStore.pages.each do |page|
+        Sem.Menu(fixed: :left, vertical:true, size: :huge, inverted: false, compact: true ) {
           PagesToc(pages: PageStore.pages)
+        }
         # end
-      }
+      #}
     # }
   end
 
   def body
-    Sem.Container(style: { marginTop: '2em', paddingLeft: '28px' }) {
-      PageStore.pages.each do |page|
-        PageBody(page: page) if page[:md_converter]
-        Sem.Divider()
+    #Sem.Container(style: { marginTop: '2em', paddingLeft: '28px' }) {
+      DIV(class: 'main-container') do
+        PageStore.pages.each do |page|
+          PageBody(page: page) if page[:md_converter]
+          Sem.Divider()
+        end
       end
-    }
+    #}
   end
 
 end
--- app/components/pages_toc.rb
+++ app/components/pages_toc.rb
@@ -7,16 +7,17 @@ class PagesToc < Hyperloop::Component
   render(DIV) do
     panels = []
     params.pages.each_with_index do |page, index|
+      puts "HEADING: #{page[:md_converter].headings[0]['text']}"
       panels << { title: TocHeading(heading: page[:md_converter].headings[0]).as_node,
                     content: panel(page).as_node,
                     key: index.to_s
       }
     end
-    Sem.Accordion(panels: panels.to_n, styled: false, fluid: true)
+    Sem.Accordion(panels: panels.to_n, styled: true, fluid: true)
   end
 
   def panel page
-    DIV do
+    UL do
       page[:md_converter].headings.drop(1).each do |heading|
         TocItem(heading: heading)
       end
@@ -36,10 +37,15 @@ end
 
 class TocItem < Hyperloop::Component
   param :heading
-  render(DIV) do
-    LI(class: "toc_h#{params.heading[:level]}") { params.heading[:text] }.on(:click) do
-      slug = "#{params.heading[:slug]}"
-      `document.getElementById(#{slug}).scrollIntoView(true);`
+  render do
+    
+
+    LI(class: "toc_h#{params.heading[:level]}") do
+        A { params.heading[:text] }.on(:click) do
+        slug = "#{params.heading[:slug]}"
+        `document.getElementById(#{slug}).scrollIntoView(true);`
+      end
     end
+
   end
 end
