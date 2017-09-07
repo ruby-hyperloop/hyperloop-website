de1d74d7577ab4e891d3a4e01a0703b9253c532e
Barrie Hadfield <barrie@workshare.com>
2017-09-07 08:06:21 +0100

    trying to get independent scrolling

--- app/components/docs/docs_body.rb
+++ app/components/docs/docs_body.rb
@@ -1,5 +1,7 @@
 class DocsBody < Hyperloop::Component
   render(DIV) do
-    H1 { "Docs body" }
+    100.times do
+      H1 { "Docs body" }
+    end
   end
 end
--- app/components/shared/page_layout.rb
+++ app/components/shared/page_layout.rb
@@ -4,27 +4,37 @@ class PageLayout < Hyperloop::Router::Component
   param :loaded
 
   render(DIV) do
-    AppMenu()
 
     Sem.Dimmer(active: (params.loaded ? false : true), inverted: true) {
       Sem.Loader(inverted: true, size: :large, content: 'Loading dynamic content...')
     }
 
-    Sem.SidebarPushable do
-      Sem.Sidebar(animation: :push, width: :wide, visible: SidebarStore.visible ) do
-        # DIV(class: 'sidenav-container') {
-        # FRED I am not sure why the style from the div is not being applied, so am using
-        # Sem.Container as a hack...
-        Sem.Container(style: { marginTop: '3.5em' }) {
-          params.sidebar_component.render
-        }
+    #  FRED cannot get this column to scroll independantly!!
+    Sem.Grid do
+      Sem.GridRow { AppMenu() }
+      Sem.GridRow(columns: '2') do
+        Sem.GridColumn do
+          Sem.SidebarPushable do
+            Sem.Sidebar(as: :Segment, animation: :push, width: :wide, visible: SidebarStore.visible ) do
+              # DIV(class: 'sidenav-container') {
+              # FRED I am not sure why the style from the div is not being applied, so am using
+              # Sem.Container as a hack...
+              Sem.Container(style: { marginTop: '3.5em' }) {
+                params.sidebar_component.render
+              }
+            end
+          end
+        end
+        Sem.GridColumn do
+          Sem.SidebarPusher do
+            Sem.Container(style: { marginTop: '3.5em'}) {
+              params.body_component.render
+            }
+          end
+        end
       end
-      Sem.SidebarPusher {
-        Sem.Container(style: { marginTop: '3.5em'}) {
-          params.body_component.render
-        }
-      }
     end
+
   end
 
 end
