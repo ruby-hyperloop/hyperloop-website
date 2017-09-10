3dc34ce1d457429250a8c8e37e377f85ad0d3222
Barrie Hadfield <barrie@workshare.com>
2017-09-10 13:08:28 +0100

    search working but with warnings

--- app/components/shared/site_search.rb
+++ app/components/shared/site_search.rb
@@ -6,7 +6,6 @@ class SiteSearch < Hyperloop::Component
     Sem.Search(category: true, aligned: :right,
       onSearchChange: lambda { |e, value| search_change(e, value) },
       resultRenderer: lambda { |obj| render_result(obj) },
-      categoryRenderer: lambda { |obj| render_category(obj) },
       results: state.results,
       loading: !SiteStore.loaded?
     )
@@ -17,15 +16,19 @@ class SiteSearch < Hyperloop::Component
     mutate.results SiteStore.search_headings(value).to_n
   end
 
-  def render_category obj
-    SPAN do
-      P { 'obj.text' }
-    end.to_n
-  end
-
   def render_result obj
-    SPAN do
-      P { `obj.text` }
-    end.to_n
+    #  FRED I don't know why we get the warnings about props on a div and a missing key
+    #  clearly this is not where to declare these props, I have tried everything
+    r=rand(2**256).to_s(36)[0..7]
+    React.create_element(SearchResult, {slug: `obj.slug`, text: `obj.text`, key: r} ).to_n
+  end
+end
+
+class SearchResult < Hyperloop::Component
+  param :slug
+  param :text
+
+  render do
+    P { params.text }
   end
 end
