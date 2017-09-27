52532a1b3cfa0f783f9a3d97c3efa72dab3d772e
Barrie Hadfield <barrie@workshare.com>
2017-09-27 11:22:40 +0100

    matching route

--- app/components/docs/docs_page.rb
+++ app/components/docs/docs_page.rb
@@ -4,13 +4,26 @@ class DocsPage < Hyperloop::Router::Component
     SidebarStore.hide
   end
 
-  render do
+  render(DIV) do
+    puts match.url
 
     sidebar = DocsSidebar(section_store: SiteStore.sections[:docs],
                           history: history,
                           section: 'docs').as_node
     body = DocsBody(section_store: SiteStore.sections[:docs]).as_node
     title = "Hyperloop Documentation"
+
+    Route('/docs/:repo', exact: true) do
+      puts "we have a repo"
+    end
+
+    Route('/docs/:repo/:file', exact: true) do
+      puts "we have a repo and file"
+    end
+
+    Route('/docs/:repo/:file/:slug', exact: true) do
+      puts "we have a repo and file and slug"
+    end
 
     PageLayout(sidebar_component: sidebar, body_component: body, page_title: title, loaded: SiteStore.sections[:docs].loaded?)
 
